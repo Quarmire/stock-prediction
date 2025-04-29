284619fd05031dc1cd611e65d03469e656c9746b
Quarmire <28873711+Quarmire@users.noreply.github.com>
2025-04-29 16:46:54 -0500

    updated shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
 let
   # pkgs = import <nixpkgs> { };
   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/5461b7fa65f3ca74cef60be837fd559a8918eaa0.tar.gz") {};
-  python = pkgs.python312;
-  pythonPackages = python.pkgs;
+  # python = pkgs.python312;
+  # pythonPackages = python.pkgs;
   lib-path = with pkgs; lib.makeLibraryPath [
     stdenv.cc.cc
   ];
@@ -21,6 +21,9 @@ in with pkgs; mkShell {
       scipy
       scikit-learn
       pymc
+      tqdm
+      jupyter-core
+      notebook
     ]))
   ];
 
@@ -31,12 +34,5 @@ in with pkgs; mkShell {
   shellHook = ''
     SOURCE_DATE_EPOCH=$(date +%s)
     export "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${lib-path}"
-    VENV=.venv
-
-    if test ! -d $VENV; then
-      python3.12 -m venv $VENV
-    fi
-    source ./$VENV/bin/activate
-    export PYTHONPATH=`pwd`/$VENV/${python.sitePackages}/:$PYTHONPATH
   '';
 }
